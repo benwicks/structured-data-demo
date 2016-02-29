09bf39d1fdfb84f6283f197f5e23104947dde37e
Ben Wicks <bwicks@buildertrend.com>
2016-02-29 09:23:19 -0600

    Initial Commit

---
+++ StructuredDataDemo/app/src/main/java/com/benjaminwicks/structureddatademo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.benjaminwicks.structureddatademo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public final class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
